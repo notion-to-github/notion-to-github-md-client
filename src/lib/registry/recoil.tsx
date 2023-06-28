dc209c9ed2d976a639ba86727a9a64c5f82f0900
Jaeyoung Kwak <66120479+julyydev@users.noreply.github.com>
2023-06-28 21:52:56 +0900

    [Feat] recoil root 컴포넌트 구현

---
+++ src/lib/registry/recoil.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { RecoilRoot } from "recoil";
+
+export default function Recoil({ children }: { children: React.ReactNode }) {
+    return <RecoilRoot>{children}</RecoilRoot>;
+}
